bac703b515bb9c44181024dee5c33d08a6549d48
Egor Dubina <belochka20022@gmail.com>
2023-02-25 15:46:55 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,77 +1,125 @@
-const navItems = document.querySelectorAll(".nav-item")
-const infoBlocks = document.querySelectorAll(".info")
-const closeBtns = document.querySelectorAll(".close-btn")
-const navBtn = document.querySelector(".nav-button")
-const closeNavBtn = document.querySelector(".nav-button-close-menu")
-const nav = document.getElementsByTagName("nav")[0]
-const animationTime = 250
+const ANIMATION_TIME = 250
 
-closeAllInfoBlocks()
+const navigationItems = document.querySelectorAll('.nav-item')
+const infoBlocks = document.querySelectorAll('.info')
+const closeInfoBlockButtons = document.querySelectorAll('.close-btn')
+const navigationButtons = document.querySelector('.nav-button')
+const closeNavigationMenuButton = document.querySelector(
+  '.nav-button-close-menu'
+)
+const navBar = document.getElementsByTagName('nav')[0]
 
-navItems.forEach((navItem) => {
-  navItem.addEventListener("click", () => {
-    closeAllInfoBlocks()
-    changeMenuVisible()
-    infoBlocks.forEach((block) => {
-      if (block.getAttribute("id") === navItem.getAttribute("id")) {
-        navItem.classList.add("active")
-        nav.classList.remove("active")
-        navBtn.classList.remove("menu-active")
-        showBlock(block)
-      }
-    })
-  })
-})
-
-closeBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    unactiveAllMenuItems()
-    closeAllInfoBlocks()
-  })
-})
-
-navBtn.addEventListener("click", () => {
-  changeMenuVisible()
-})
-
-closeNavBtn.addEventListener("click", () => {
-  changeMenuVisible()
-})
-
+// Убирает актив со всех пунктов меню
 function unactiveAllMenuItems() {
-  navItems.forEach((navItem) => {
-    navItem.classList.remove("active")
+  navigationItems.forEach((navItem) => {
+    navItem.classList.remove('active')
   })
 }
 
+// Закрывает все блоки информации
 function closeAllInfoBlocks() {
   unactiveAllMenuItems()
+  const svgObject = document.querySelector('object').contentDocument
+  const areas = svgObject.querySelectorAll('.area')
+  areas.forEach((area) => {
+    area.classList.remove('active')
+  })
   infoBlocks.forEach((block) => {
-    block.style.transform = "translateX(-100%)"
-    setTimeout(() => {
-      block.style.display = "none"
-    }, animationTime)
+    if (block.style.transform !== 'translateX(-100%)') {
+      block.style.transform = 'translateX(-100%)'
+      setTimeout(() => {
+        block.style.display = 'none'
+      }, ANIMATION_TIME)
+    }
   })
 }
 
-function showBlock(block) {
+function showBlock(id) {
+  closeAllInfoBlocks()
   setTimeout(() => {
-    block.style.display = "block"
-    setTimeout(() => {
-      block.style.transform = "translateX(0)"
-    }, animationTime)
-  }, animationTime)
+    infoBlocks.forEach((block) => {
+      if (block.getAttribute('id') === id) {
+        block.style.display = 'block'
+        block.style.position = 'absolute'
+        block.style.top = 0
+        block.style.left = 0
+        setTimeout(() => {
+          block.style.transform = 'translateX(0)'
+        }, ANIMATION_TIME)
+      }
+    })
+  }, ANIMATION_TIME)
 }
 
 function changeMenuVisible() {
-  if (nav.classList.contains("active")) {
-    nav.classList.remove("active")
-    navBtn.classList.remove("menu-active")
-    closeNavBtn.classList.remove("menu-active")
+  if (navBar.classList.contains('active')) {
+    navBar.classList.remove('active')
+    navigationButtons.classList.remove('menu-active')
+    closeNavigationMenuButton.classList.remove('menu-active')
   } else {
     closeAllInfoBlocks()
-    nav.classList.add("active")
-    navBtn.classList.add("menu-active")
-    closeNavBtn.classList.add("menu-active")
+    navBar.classList.add('active')
+    navigationButtons.classList.add('menu-active')
+    closeNavigationMenuButton.classList.add('menu-active')
   }
 }
+
+window.addEventListener('load', () => {
+  // работа с картой
+  const svgObject = document.querySelector('object').contentDocument
+  const areas = svgObject.querySelectorAll('.area')
+  areas.forEach((area) => {
+    area.addEventListener('click', () => {
+      closeAllInfoBlocks()
+      areas.forEach((area) => {
+        area.classList.remove('active')
+      })
+      showBlock(area.getAttribute('id'))
+      area.classList.add('active')
+      navigationItems.forEach((navItem) => {
+        if (navItem.getAttribute('id') === area.getAttribute('id')) {
+          navItem.classList.add('active')
+        }
+      })
+    })
+  })
+
+  closeAllInfoBlocks()
+
+  navigationItems.forEach((navItem) => {
+    const svgObject = document.querySelector('object').contentDocument
+    const areas = svgObject.querySelectorAll('.area')
+    navItem.addEventListener('click', () => {
+      closeAllInfoBlocks()
+      changeMenuVisible()
+      infoBlocks.forEach((block) => {
+        if (block.getAttribute('id') === navItem.getAttribute('id')) {
+          navItem.classList.add('active')
+          navBar.classList.remove('active')
+          navigationButtons.classList.remove('menu-active')
+          showBlock(block.getAttribute('id'))
+        }
+      })
+      areas.forEach((area) => {
+        if (area.getAttribute('id') === navItem.getAttribute('id')) {
+          area.classList.add('active')
+        }
+      })
+    })
+  })
+
+  closeInfoBlockButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      unactiveAllMenuItems()
+      closeAllInfoBlocks()
+    })
+  })
+
+  navigationButtons.addEventListener('click', () => {
+    changeMenuVisible()
+  })
+
+  closeNavigationMenuButton.addEventListener('click', () => {
+    changeMenuVisible()
+  })
+})
